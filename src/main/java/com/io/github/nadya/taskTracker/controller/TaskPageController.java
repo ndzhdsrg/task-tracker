7f215515530e06c5a19c9d478ce380d81661fa14
«Надежда» <yureva.n@finfort.ru>
2026-04-12 22:19:15 +0300

    Добавлено:  Первые request/response DTO  Добавлен сервисный метод  Написана реализация метода создания  Написан класс с контроллером, который позволяет обрабатывать http-запрос  Добавлена аннотация для поднятия api doc  Начинаю оформлять read me
     Задачи:
     1. Записать алгоритм создания crud-действий полностью
     2. Сделать обработку ответов с ошибкой
     3. Сделать сохранение данных, добавленных в БД?
     4. Составить общий план по реализации задания
     5. Реализовать задание
     6. Оформить проект

--- src/main/java/com/io/github/nadya/taskTracker/controller/TaskPageController.java
+++ src/main/java/com/io/github/nadya/taskTracker/controller/TaskPageController.java
@@ -1,5 +1,6 @@
 package com.io.github.nadya.taskTracker.controller;
 
+import com.io.github.nadya.taskTracker.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TaskPageController {
 
+    private final TaskService taskService;
+
+    public TaskPageController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @GetMapping("/tasks-ui")
     public String getTasksPage(Model model) {
+        model.addAttribute("task", taskService.returnAllTasksList());
         return "task"; // имя HTML файла без .html
     }
 }
